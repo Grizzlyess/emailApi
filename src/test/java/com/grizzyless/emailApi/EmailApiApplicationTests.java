49024165b9d94eb141d508d4eaaa5b515b82ce4a
Clara <mariaclarab316@gmail.com>
2026-06-24 15:12:50 -0300

    first commit create project

---
+++ src/test/java/com/grizzyless/emailApi/EmailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grizzyless.emailApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
