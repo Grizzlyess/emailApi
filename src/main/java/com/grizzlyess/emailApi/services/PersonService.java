5146431d21330ed62356569653e518afe77fc089
Clara <mariaclarab316@gmail.com>
2026-07-01 14:56:42 -0300

    Implementing create from CRUDn

--- src/main/java/com/grizzlyess/emailApi/services/PersonService.java
+++ src/main/java/com/grizzlyess/emailApi/services/PersonService.java
@@ -1,16 +1,29 @@
 package com.grizzlyess.emailApi.services;
 
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
 import com.grizzlyess.emailApi.entity.Person;
+import com.grizzlyess.emailApi.enums.DepartmentType;
 import com.grizzlyess.emailApi.repository.PersonRepository;
 
+@Service
 public class PersonService {
+    private static final Logger log = LoggerFactory.getLogger(PersonService.class);
     public PersonRepository personRepository;
 
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public Person create(Person person) {
+    public Person create(String firstName, String lastName, DepartmentType department) {
+
+        String email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department.toString().toLowerCase() + ".com";
+        String password = UUID.randomUUID().toString().substring(0, 8);
+        Person person = new Person(firstName, lastName, department, email, password);
         return personRepository.save(person);
     }
 
